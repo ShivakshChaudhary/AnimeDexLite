eb29da660d4f553acc6ad6c9fdcf4f0ac3248d63
Shivaksh Chaudhary <shivakshchaudhary2010@gmail.com>
2025-06-27 21:56:01 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const recentapi = "/recent/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://api100.anime-dex.workers.dev"];
+const AvailableServers = ["https://api.zylora.me"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
